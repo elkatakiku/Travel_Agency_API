23d86c1e6776800dabe42234d65ff98fe867e83b
elkatakiku <79001808+elkatakiku@users.noreply.github.com>
2023-06-18 16:06:45 +0800

    Add public endpoint with pagination and tests

--- database/factories/TravelFactory.php
+++ database/factories/TravelFactory.php
@@ -17,7 +17,10 @@ class TravelFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'is_public' => fake()->boolean,
+            'name' => fake()->text(20),
+            'description' => fake()->text(100),
+            'number_of_days' => rand(1, 10),
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::prefix('v1')->group(function () {
+//
+//});
+
+Route::get('travels', [\App\Http\Controllers\Api\V1\TravelController::class, 'index']);
