964b72e15d80601f4e1c33a976321dfed000498c
elkatakiku <79001808+elkatakiku@users.noreply.github.com>
2023-06-18 15:05:29 +0800

    Add model and migrations

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,6 +20,10 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
+    protected $observers = [
+
+    ];
+
     /**
      * Register any events for your application.
      */
